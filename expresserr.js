4a3a1a439def5c120aeb601311e5038168a92faa
shashikant-op <sharmashashikantqwe@gmail.com>
2024-08-22 23:39:00 +0530

    err handle

---
+++ expresserr.js
@@ -0,0 +1,9 @@
+class expresserr extends Error{
+    constructor(status,message){
+        super();
+        this.status=status;
+        this.message=message;
+    }
+}
+
+module.exports=expresserr;
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+const expresserr=require("./expresserr.js");
+
 main()
 .then(()=>{
     console.log("mongodb connected");
@@ -24,39 +26,57 @@ app.get("/",(req,res)=>{
     res.send("this is home route");
 })
 
-app.get("/chats",async(req,res)=>{
-    let chats=await chat.find();
+app.get("/chats",async(req,res,next)=>{
+    try{
+         let chats=await chat.find();
     res.render("index.ejs",{chats});
+    } catch(err){
+        next(err);
+    };
+   
 })
 //new route
 app.get("/chats/new",(req,res)=>{
    res.render("new.ejs");
 })
 //create route
-app.post("/chats",(req,res)=>{
-    let{from,to,msg}=req.body;
-    let newchat=new chat({
-        from:from,
-        to:to,
-        msg:msg,
-        createdat:Date(),
-
-    })
-    newchat.save().then((res)=>{console.log("chat was saved")}).catch((err)=>{
-        console.log(err);
-    })
-    res.redirect("/chats");
+app.post("/chats",(req,res,next)=>{
+    try{
+        let{from,to,msg}=req.body;
+        let newchat=new chat({
+            from:from,
+            to:to,
+            msg:msg,
+            createdat:Date(),
+     })
+        newchat.save().then((res)=>{console.log("chat was saved")}).catch((err)=>{
+            console.log(err);
+        })
+        res.redirect("/chats");
+    }catch(err){
+        next(err);
+    }
+        
 })
 //edit route
-app.get("/chats/:id/edit",async(req,res)=>{
-   let {id}=req.params;
+app.get("/chats/:id/edit",async(req,res,next)=>{
+   try{
+    let {id}=req.params;
    let chats = await chat.findById(id);
+   if(!chats){
+   next( new expresserr(300,"id is not found"));
+   }
    res.render("edit.ejs",{chats});
+   }catch(err){
+    next(err);
+   }
+   
 })
 
 //update route
-app.put("/chats/:id",async(req,res)=>{
-    let {id}=req.params;
+app.put("/chats/:id",async(req,res,next)=>{
+    try{
+         let {id}=req.params;
     let{msg:newMsg}=req.body;
     console.log(newMsg)
     let updatedchat=await chat.findByIdAndUpdate(
@@ -64,14 +84,32 @@ app.put("/chats/:id",async(req,res)=>{
          {runValidators:true,new:true});
          console.log(updatedchat);
          res.redirect("/chats");
+    }catch(err){
+        next(err);
+    }
+   
 })
 //delete route
-app.delete("/chats/:id",async(req,res)=>{
-    let {id}=  req.params;
+app.delete("/chats/:id",async(req,res,next)=>{
+    try{
+        let {id}=  req.params;
     await chat.findByIdAndDelete(id);
     res.redirect("/chats");
     console.log("chat was deleted");
+    }catch(err){
+        next(err);
+    }
+    
+    
 })
+
+//err handeling
+app.use((err,req,res,next)=>{
+    let{status=400,message="some err is generate"}=err;
+    res.status(status).send(message);
+    console.log(message);
+})
+
 app.listen(8080,()=>{
     console.log("app is listning on port 8080");
 })
